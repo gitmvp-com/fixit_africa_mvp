1ea0a134e3ab75ca0b4479e1d0ff432fd82d7b0d
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:39 +0300

    Add feature screens - Part 5 (Admin Features)

---
+++ lib/presentation/enhanced_marketplace_screen/enhanced_marketplace_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EnhancedMarketplaceScreen extends StatelessWidget {
+  const EnhancedMarketplaceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Enhanced Marketplace')),
+      body: const Center(child: Text('Enhanced Marketplace Screen')),
+    );
+  }
+}
