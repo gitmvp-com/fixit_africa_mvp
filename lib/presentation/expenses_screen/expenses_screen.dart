6eb76f4f99604bca2d01a955ad871a068ba5d0ed
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:16 +0300

    Add feature screens - Part 4 (Analytics, Inventory Management)

---
+++ lib/presentation/expenses_screen/expenses_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ExpensesScreen extends StatelessWidget {
+  const ExpensesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Expenses')),
+      body: const Center(child: Text('Expenses Screen')),
+    );
+  }
+}
