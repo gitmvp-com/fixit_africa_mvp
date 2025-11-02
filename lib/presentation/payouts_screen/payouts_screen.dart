316ddb26d18e726dcbbb714e5c8183556905246d
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:00 +0300

    Add feature screens - Part 3 (Mechanic Management, Financial)

---
+++ lib/presentation/payouts_screen/payouts_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PayoutsScreen extends StatelessWidget {
+  const PayoutsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Payouts')),
+      body: const Center(child: Text('Payouts Screen')),
+    );
+  }
+}
