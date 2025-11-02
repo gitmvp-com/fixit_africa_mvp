fd80b382406332367129fb53e9963a240c8c817e
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:17 +0300

    Add feature screens - Part 4 (Analytics, Inventory Management)

---
+++ lib/presentation/top_mechanics_garages_screen/top_mechanics_garages_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TopMechanicsGaragesScreen extends StatelessWidget {
+  const TopMechanicsGaragesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Top Mechanics & Garages')),
+      body: const Center(child: Text('Top Mechanics & Garages Screen')),
+    );
+  }
+}
