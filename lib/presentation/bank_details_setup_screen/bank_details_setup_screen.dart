fbdc147555eb888e3219c296a109f0a14cf9ec03
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:15:57 +0300

    Add feature screens - Part 3 (Mechanic Management, Financial)

---
+++ lib/presentation/bank_details_setup_screen/bank_details_setup_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BankDetailsSetupScreen extends StatelessWidget {
+  const BankDetailsSetupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bank Details Setup')),
+      body: const Center(child: Text('Bank Details Setup Screen')),
+    );
+  }
+}
