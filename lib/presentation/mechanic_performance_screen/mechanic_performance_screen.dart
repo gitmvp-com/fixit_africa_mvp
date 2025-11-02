9d8cd374b023c1abe6b7777f29829c1c3dcd27f2
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:15:58 +0300

    Add feature screens - Part 3 (Mechanic Management, Financial)

---
+++ lib/presentation/mechanic_performance_screen/mechanic_performance_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MechanicPerformanceScreen extends StatelessWidget {
+  const MechanicPerformanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mechanic Performance')),
+      body: const Center(child: Text('Mechanic Performance Screen')),
+    );
+  }
+}
