ef1285ee7848d4908d37cd6a0827447ea1607d2d
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:56 +0300

    Add final feature screens and assets directory

---
+++ lib/presentation/gift_cards_referrals_screen/gift_cards_referrals_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GiftCardsReferralsScreen extends StatelessWidget {
+  const GiftCardsReferralsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Gift Cards & Referrals')),
+      body: const Center(child: Text('Gift Cards & Referrals Screen')),
+    );
+  }
+}
