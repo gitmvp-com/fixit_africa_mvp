c588319574338262d59b3bfd41ca51d776f0ec0f
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:20 +0300

    Add feature screens - Part 4 (Analytics, Inventory Management)

---
+++ lib/presentation/dynamic_pricing_screen/dynamic_pricing_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DynamicPricingScreen extends StatelessWidget {
+  const DynamicPricingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Dynamic Pricing')),
+      body: const Center(child: Text('Dynamic Pricing Screen')),
+    );
+  }
+}
