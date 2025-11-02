d14b2000967df6b5255d8bbc99e0ac8e4400a268
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:15:55 +0300

    Add feature screens - Part 3 (Mechanic Management, Financial)

---
+++ lib/presentation/mechanic_onboarding_screen/mechanic_onboarding_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MechanicOnboardingScreen extends StatelessWidget {
+  const MechanicOnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mechanic Onboarding')),
+      body: const Center(child: Text('Mechanic Onboarding Screen')),
+    );
+  }
+}
