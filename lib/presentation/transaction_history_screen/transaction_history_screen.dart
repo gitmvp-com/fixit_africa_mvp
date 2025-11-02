25f4b8e738307a698c4c08b0316df5cb2ce6a359
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:15:59 +0300

    Add feature screens - Part 3 (Mechanic Management, Financial)

---
+++ lib/presentation/transaction_history_screen/transaction_history_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistoryScreen extends StatelessWidget {
+  const TransactionHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Transaction History')),
+      body: const Center(child: Text('Transaction History Screen')),
+    );
+  }
+}
