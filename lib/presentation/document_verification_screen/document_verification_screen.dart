6f912f2b7d72211daf6c38b0a42a83499015570c
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:15:57 +0300

    Add feature screens - Part 3 (Mechanic Management, Financial)

---
+++ lib/presentation/document_verification_screen/document_verification_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DocumentVerificationScreen extends StatelessWidget {
+  const DocumentVerificationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Document Verification')),
+      body: const Center(child: Text('Document Verification Screen')),
+    );
+  }
+}
