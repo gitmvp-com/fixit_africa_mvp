6c1d3f4fd13f7f06724a7bf4fd5250aaf4cf2d86
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:19 +0300

    Add feature screens - Part 4 (Analytics, Inventory Management)

---
+++ lib/presentation/bulk_update_screen/bulk_update_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BulkUpdateScreen extends StatelessWidget {
+  const BulkUpdateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bulk Update')),
+      body: const Center(child: Text('Bulk Update Screen')),
+    );
+  }
+}
