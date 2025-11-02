b161b43129f032f318a2f96c681fcbeae2124183
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:19 +0300

    Add feature screens - Part 4 (Analytics, Inventory Management)

---
+++ lib/presentation/bulk_edit_screen/bulk_edit_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BulkEditScreen extends StatelessWidget {
+  const BulkEditScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Bulk Edit')),
+      body: const Center(child: Text('Bulk Edit Screen')),
+    );
+  }
+}
