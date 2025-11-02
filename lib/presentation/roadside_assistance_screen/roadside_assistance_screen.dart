51ca98c34b15631f6e6e4805fc9cdb7d753546c4
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:15:37 +0300

    Add feature screens - Part 2 (AI, Navigation, Mechanic Signup)

---
+++ lib/presentation/roadside_assistance_screen/roadside_assistance_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RoadsideAssistanceScreen extends StatelessWidget {
+  const RoadsideAssistanceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Roadside Assistance')),
+      body: const Center(child: Text('Roadside Assistance Screen')),
+    );
+  }
+}
