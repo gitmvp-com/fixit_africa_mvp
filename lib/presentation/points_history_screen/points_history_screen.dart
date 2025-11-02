c50d6acbad1c029157922310b64363b27f9c1a08
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:39 +0300

    Add feature screens - Part 5 (Admin Features)

---
+++ lib/presentation/points_history_screen/points_history_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PointsHistoryScreen extends StatelessWidget {
+  const PointsHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Points History')),
+      body: const Center(child: Text('Points History Screen')),
+    );
+  }
+}
