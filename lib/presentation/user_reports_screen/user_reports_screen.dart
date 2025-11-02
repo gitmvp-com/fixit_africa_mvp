194bd3b18c6698e251ef1bbaa463be9217c0274b
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:37 +0300

    Add feature screens - Part 5 (Admin Features)

---
+++ lib/presentation/user_reports_screen/user_reports_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserReportsScreen extends StatelessWidget {
+  const UserReportsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('User Reports')),
+      body: const Center(child: Text('User Reports Screen')),
+    );
+  }
+}
