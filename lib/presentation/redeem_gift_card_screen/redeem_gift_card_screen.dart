c9c29779994b3b9c7738faa85a9d41d814de3cb4
Fili <126945152+filiksyos@users.noreply.github.com>
2025-11-02 16:16:57 +0300

    Add final feature screens and assets directory

---
+++ lib/presentation/redeem_gift_card_screen/redeem_gift_card_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RedeemGiftCardScreen extends StatelessWidget {
+  const RedeemGiftCardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Redeem Gift Card')),
+      body: const Center(child: Text('Redeem Gift Card Screen')),
+    );
+  }
+}
